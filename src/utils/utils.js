4d17d047b0fd0f7598d437ac6078418509c2aef9
DK_Peng <1063137960@qq.com>
2018-11-01 11:31:24 +0800

    add: 数组去重

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -30,5 +30,22 @@ export default {
     var colorArray = ['#34c7da', '#96d2ed', '#3498d9', '#f3ad40', '#1abc9b', '#39435f', '#34b8d9', '#b07a4b', '#b07a4b', '#5bb389']
     var index = Math.floor(Math.random() * (colorArray.length - 1))
     return colorArray[index]
-  }
+  },
+  /**
+   * desc: 数组去重
+   * @param {Array} array
+   * @return {String}
+  */
+  uniqueArray: (arr) => {
+    let newArray = []
+    let obj = {}
+
+    for (let i = 0; i < arr.length; i++) {
+      if (!obj[arr[i]]) {
+        obj[arr[i]] = true
+        newArray.push(arr[i])
+      }
+    }
+    return newArray
+  },
 }
