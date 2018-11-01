93f1b1c679accd8d1b26d4f4a7abccb9b87b251b
DK_Peng <1063137960@qq.com>
2018-11-01 10:05:12 +0800

    add: 新增饼图;

--- src/view/Manage/ErekHome/Home.vue
+++ src/view/Manage/ErekHome/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="vue-erek-home-container">
+    <div class="vue-erek-home-up">
       <vue-erek-echarts-line
         :xAxis='xAxis'
         :yAxis='yAxis'
-        :series='series'
+        :series='lineSeries'
         :itemList='itemList'
-        :width='width'
-        :height='height'
+        :width='lineWidth'
+        :height='lineHeight'
       ></vue-erek-echarts-line>
       <div class="vue-erek-right-box" :style="{height: 'height'}">
         <p class='vue-erek-right-box-title'>数据卡片
@@ -15,24 +15,39 @@
         <vue-erek-tab :tabArray='tabArray'/>
       </div>
     </div>
+    <erek-br :bgColor='hrObj.bgColor' :height='hrObj.height'></erek-br>
+    <div class="vue-erek-home-down">
+      <vue-erek-echarts-pie
+        :series="pieSeries"
+        :width='pieWidth'
+        :height='pieHeight'
+      ></vue-erek-echarts-pie>
+      <vue-erek-echarts-pie
+        :series="pieSeries"
+        :width='pieWidth'
+        :height='pieHeight'
+      ></vue-erek-echarts-pie>
+    </div>
   </div>
 </template>
 
 <script>
 import VueErekEchartsLine from '../../../common/ErekEcharts/erek-line.vue'
+import VueErekEchartsPie from '../../../common/ErekEcharts/erek-pie.vue'
 import VueErekTab from '../../../common/ErekTab/vue-erek-tab.vue'
+import ErekBr from '../../../common/ErekBr/vue-erek-br.vue'
 import tabconfig from '../../../config/tab'
 
 export default {
   name: 'ErekManageHome',
   components: {
     VueErekEchartsLine,
-    VueErekTab
+    VueErekEchartsPie,
+    VueErekTab,
+    ErekBr
   },
   data () {
     return {
-      width: '100%',
-      height: '300px',
       xAxis: { // x轴 
         data: []
       }, 
@@ -41,8 +56,17 @@ export default {
         max: null
       },
       itemList: [],
-      series: [], // 折线数据
-      tabArray: [],
+      lineWidth: '100%',
+      lineHeight: '300px',
+      lineSeries: [], // 折线数据
+      tabArray: [], // 数据卡片
+      pieSeries: [], // 饼图数据
+      pieWidth: '100%',
+      pieHeight: '300px',
+      hrObj: {
+        bgColor: '#f5f7f9',
+        height: '30px'
+      },
     }
   },
   mounted() {
@@ -74,11 +98,11 @@ export default {
           },
           smooth: true // 折线 圆滑
         }
-        this.series.push(obj)
+        this.lineSeries.push(obj)
       }
     })
+    // 请求拿到 `卡片数据`
     this.$api.fetchAllTabData().then(res => {
-      console.log(res)
       for (let q = 0; q < res.length; q++) {
         let bgColor = this.$utils.getColorFromArray()
         let conf = Object.assign({}, tabconfig, {
@@ -93,12 +117,24 @@ export default {
         this.tabArray.push(conf)
       }
     })
+    // 请求拿到 `站点访问来源数据`
+    this.$api.fetchAllOriginData().then(res => {
+      let obj = {
+        name: '访问来源',
+        type: 'pie',
+        data: []
+      }
+      for (let k = 0; k < res.length; k++) {
+        obj.data.push(res[k])
+      }
+      this.pieSeries.push(obj)
+    })
   },
 }
 </script>
 
 <style scoped lang='scss'>
-.vue-erek-home-container {
+.vue-erek-home-up {
   width: 100%;
   display: flex;
   flex: 1;
@@ -116,5 +152,11 @@ export default {
       margin-top: 10px;
     }
   }
+}
+.vue-erek-home-down {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
 }
 </style>
