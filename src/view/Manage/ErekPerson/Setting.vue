4abbe4436b31c659c1dac0d03d0964ea6cd89111
DK_Peng <1063137960@qq.com>
2018-11-05 11:20:35 +0800

    add: 完成个人设置中的基本设置

--- src/view/Manage/ErekPerson/Setting.vue
+++ src/view/Manage/ErekPerson/Setting.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>个人设置</div>
+  <div class="vue-erek-person-setting-container">
+    <Tabs size='small'>
+        <TabPane label="基本设置">
+          <erek-base-setting></erek-base-setting>
+        </TabPane>
+        <TabPane label="安全设置">
+          <erek-safe-setting></erek-safe-setting>
+          </TabPane>
+        <TabPane label="账号绑定">
+          <erek-account-setting></erek-account-setting>
+          </TabPane>
+        <TabPane label="新消息通知">
+          <erek-message-notice></erek-message-notice>
+        </TabPane>
+    </Tabs>
+  </div>
 </template>
 
 <script>
+import ErekBaseSetting from '../../../common/ErekPersonTabs/vue-erek-base-setting.vue'
+import ErekSafeSetting from '../../../common/ErekPersonTabs/vue-erek-safe-setting.vue'
+import ErekAccountSetting from '../../../common/ErekPersonTabs/vue-erek-account-setting.vue'
+import ErekMessageNotice from '../../../common/ErekPersonTabs/vue-erek-message-notice.vue'
 export default {
-
+  name: 'ErekPersonSetting',
+  components: {
+    ErekBaseSetting,
+    ErekSafeSetting,
+    ErekAccountSetting,
+    ErekMessageNotice
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang='scss'>
+.vue-erek-person-setting-container {
+  padding: 16px 22px;
+}
 </style>
