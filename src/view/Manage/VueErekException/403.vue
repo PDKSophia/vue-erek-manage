68b1deeb4747eb2f63da4ca455cf57a1997afbc6
DK_Peng <1063137960@qq.com>
2018-11-17 01:17:36 +0800

    feat: add new exception page

---
+++ src/view/Manage/VueErekException/403.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>403错误</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/view/Manage/VueErekException/500.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>500错误</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
