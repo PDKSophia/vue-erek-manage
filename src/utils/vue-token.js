626ab9aa79c3d30769a534e86c5833f0f83e29c0
DK_Peng <1063137960@qq.com>
2018-11-16 20:22:31 +0800

    feat: add new sessionStorage for email code

--- src/utils/vue-token.js
+++ src/utils/vue-token.js
@@ -5,3 +5,11 @@ export function getAuthorityToken () {
 export function setAuthorityToken (vueToken) {
   return sessionStorage.setItem('vue-erek-token', vueToken)
 }
+
+export function getEmailCode () {
+  return sessionStorage.getItem('vue-erek-code')
+}
+
+export function setEmailCode (code) {
+  return sessionStorage.setItem('vue-erek-code', code)
+}
