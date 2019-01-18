36de0eb977d1e0e66a05c4685b777e8b88ea509b
DK_Peng <1063137960@qq.com>
2019-01-18 22:05:34 +0800

    feat: 新增无内容显示组件

---
+++ src/components/NoContentComponents/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="erek-no-content-container">暂无数据</div>
+</template>
+
+<script>
+export default {
+  name: 'erekNoContent'
+};
+</script>
+
+<style scoped lang="scss">
+.erek-no-content-container {
+  padding: 60px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 14px;
+}
+</style>
